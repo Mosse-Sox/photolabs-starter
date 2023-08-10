fd7e8460b94c781a85f222d00462cad7b8553e05
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-09 19:28:25 -0600

    adds a single photo component and its styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,24 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } =
+    props.sampleDataForPhotoListItem;
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <article className="photo-list__item" key={id}>
+      <img src={imageSource} className="photo-list__image"></img>
+      <div className="photo-list__user-details">
+        <img src={profile} className="photo-list__user-profile"></img>
+        <div className="photo-list__user-info">
+          <span>{username}</span>
+          <span className="photo-list__user-location">
+            {location.city}, {location.country}
+          </span>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default PhotoListItem;
