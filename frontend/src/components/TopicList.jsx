c4c4d867650ed47091b7e165268fa9b2ab30fccb
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-10 16:14:21 -0600

    adds mock data for photos and topics from seperate data files

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,37 +2,10 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-  {
-    id: "4",
-    slug: "topic-3",
-    title: "Food",
-  },
-  {
-    id: "5",
-    slug: "topic-3",
-    title: "Art",
-  },
-];
+import topics from "../mocks/topics";
 
 const TopicList = () => {
-  const topicList = sampleDataForTopicList.map((topic) => {
+  const topicList = topics.map((topic) => {
     return (<TopicListItem topicTitle={topic.title} key={topic.id}/>);
   });
 
