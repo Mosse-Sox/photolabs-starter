fdc6d4bbf3c8556273e3ce35c00007e2ca461765
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-10 19:38:14 -0600

    adds functionality to top heart, it fills in and shows a circle when there is a liked photo

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,13 +4,23 @@ import "../styles/TopNavigationBar.scss";
 import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+
+  let likedPhotoExists = false;
+
+  for (const photoId in props.like) {
+    if (props.like[photoId]) {
+      likedPhotoExists = true;
+      break;
+    }
+  }
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <div className="top-nav-bar__topic-groups">
         <TopicList />
-        <FavBadge />
+        <FavBadge isFavPhotoExist={likedPhotoExists}/>
       </div>
     </div>
   );
