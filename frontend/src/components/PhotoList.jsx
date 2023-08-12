3b50225a4f9db459afa4c25dd70aed815cbb2957
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-11 18:55:32 -0600

    gives modals photo information for rendering

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,27 +3,29 @@ import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 import photos from "../mocks/photos";
 
-
-
 const PhotoList = (props) => {
   const photoList = photos.map((photo) => {
-    const {id, location, urls, user} = photo;
+    const { id, location, urls, user, similarPhotos } = photo;
     const photoObj = {
       location,
       imageSource: urls.regular,
       username: user.name,
       profile: user.profile,
-      id
+      id,
+      similarPhotos,
     };
-    return (<PhotoListItem photoItem={photoObj} key={id} like={props.like} likePhoto={props.likePhoto} isClicked={props.isClicked}/>);
+    return (
+      <PhotoListItem
+        photoItem={photoObj}
+        key={id}
+        like={props.like}
+        likePhoto={props.likePhoto}
+        isClicked={() => props.isClicked(urls.regular, similarPhotos)}
+      />
+    );
   });
 
-
-  return (
-    <ul className="photo-list">
-      {photoList}
-    </ul>
-  );
+  return <ul className="photo-list">{photoList}</ul>;
 };
 
 export default PhotoList;
