2e9ecc1cd947f5b8277aa66d3a0e5d830bee1e28
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-12 13:53:56 -0600

    refactors useLike to use useReducder for cleaner state management of likes

--- frontend/src/hooks/useLike.js
+++ frontend/src/hooks/useLike.js
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+// Action types
+const actionTypes = {
+  ADD_FAVORITE: "ADD_FAVORITE",
+  REMOVE_FAVORITE: "REMOVE_FAVORITE",
+};
+
+// Reducer function
+const likeReducer = (state, action) => {
+  let newState;
+  switch (action.type) {
+  case actionTypes.ADD_FAVORITE:
+    return {
+      ...state,
+      [action.photoId]: true,
+    };
+  case actionTypes.REMOVE_FAVORITE:
+    newState = { ...state };
+    delete newState[action.photoId];
+    return newState;
+  default:
+    return state;
+  }
+};
 
 const useLike = () => {
-  const [like, setLike] = useState({});
+  const [like, dispatch] = useReducer(likeReducer, {});
 
   const toggleLike = (photoId) => {
-    setLike((prevLikes) => ({
-      ...prevLikes,
-      [photoId]: !prevLikes[photoId] || false,
-    }));
+    if (like[photoId]) {
+      dispatch({ type: actionTypes.REMOVE_FAVORITE, photoId });
+    } else {
+      dispatch({ type: actionTypes.ADD_FAVORITE, photoId });
+    }
   };
 
   return {
